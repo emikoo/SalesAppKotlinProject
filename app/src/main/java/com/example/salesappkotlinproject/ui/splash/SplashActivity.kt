dfe147c5a2154b5f870f3e60af79aabc3676cf3c
emikoo <emikoo@users.noreply.github.com>
2021-05-27 12:55:52 +0600

    splash activity with anim

---
+++ app/src/main/java/com/example/salesappkotlinproject/ui/splash/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.example.salesappkotlinproject.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import com.example.salesappkotlinproject.R
+import com.example.salesappkotlinproject.ui.owner.main.MainOwnerActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        setupAnimationText()
+        setDelay()
+    }
+
+    private fun setupAnimationText() {
+        val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        splash_title.startAnimation(animation)
+        splash_subtitle.startAnimation(animation)
+    }
+
+    private fun setDelay() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            openActivity()
+        }, 1000)
+    }
+
+    private fun openActivity() {
+        val intent = Intent(this, MainOwnerActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
